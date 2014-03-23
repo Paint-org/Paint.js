7791265fcabf9a5fdcb04286d520b12912479a88
Daniele Di Sarli <danieleds0@gmail.com>
2014-03-23 16:53:19 +0100

    Aggiunti setters

--- src/classes/Brush.ts
+++ src/classes/Brush.ts
@@ -3,16 +3,24 @@
 export class Brush {
     
     private $ : JQueryStatic;
+    private _width = 1;
+    private _color = "#000000";
     
     constructor($ : JQueryStatic) {
         this.$ = $;
     }
     
     get width():number {
-        return 3;
+        return this._width;
+    }
+    set width(value:number) {
+        this._width = value;
     }
     
     get color():string {
-        return "#000000";
+        return this._color;
+    }
+    set color(value:string) {
+        this._color = value;
     }
 }
