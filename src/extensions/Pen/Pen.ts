385f688e7894c80d2487ec240f810ed088b4fe26
Daniele Di Sarli <danieleds0@gmail.com>
2014-04-02 19:35:23 +0200

    La Pen ora funziona decentemente (utilizza quadratiche di Bézier)

--- src/extensions/Pen/Pen.ts
+++ src/extensions/Pen/Pen.ts
@@ -3,13 +3,15 @@ import color = require('../../classes/Color');
 import drawTool = require('../../classes/DrawingTool');
 import point = require('../../classes/Point');
 import paper = require('../../classes/Paper');
+import paperLayer = require('../../classes/PaperLayer');
    
 export class Pen extends drawTool.DrawingTool
 {
     static EXTENSION_NAME : string = "Pen";
     paint : glob.Paint;
     
-    private _lastPt : point.Point = null;
+    private _layer : paperLayer.PaperLayer;
+    private _points : point.Point[] = [];
     
     public constructor(paint:glob.Paint) {
         super(paint);
@@ -27,12 +29,15 @@ export class Pen extends drawTool.DrawingTool
     onStartDrawing(paper:paper.Paper, point:point.Point) {
         super.onStartDrawing(paper, point);
         
-        this._lastPt = null;
+        this._points = [];
         
-        var context = paper.baseLayer.getContext();
+        this._layer = paper.addLayer(null);
+        var context = this._layer.getContext();
+        
         context.lineWidth = this.paint.toolSize;
         context.strokeStyle = this.paint.primaryColor.HexString;
-        context.beginPath();
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
         
         this.onDraw(this.paint.currentPaper, point);
     }
@@ -40,28 +45,48 @@ export class Pen extends drawTool.DrawingTool
     onDraw(paper:paper.Paper, point:point.Point) {
         super.onDraw(paper, point);
         
-        var context = paper.baseLayer.getContext();
+        var context = this._layer.getContext();
+        var canvas = this._layer.canvas;
         
-        if (this._lastPt !== null) {
-            // Iniziamo un nuovo path e ci posizioniamo sull'ultimo punto disegnato
-            context.closePath();
-            context.beginPath();
-            context.moveTo(this._lastPt.X, this._lastPt.Y);
+        this._points.push(point);
+        
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        context.beginPath();
+        
+        var len = this._points.length;
+        
+        if(len == 1) {
+            // FIXME Draw a dot 
+            
+        } else if(len == 2) {
+            context.moveTo(this._points[0].X, this._points[0].Y);
+            context.lineTo(point.X, point.Y);
+            
+        } else {
+           
+            // move to the first point
+            context.moveTo(this._points[0].X, this._points[0].Y);
+            
+            for (var i = 1; i < len - 2; i++) {
+                var xc = (this._points[i].X + this._points[i + 1].X) / 2;
+                var yc = (this._points[i].Y + this._points[i + 1].Y) / 2;
+                context.quadraticCurveTo(this._points[i].X, this._points[i].Y, xc, yc);
+            }
+            
+            // curve through the last two points
+            context.quadraticCurveTo(this._points[i].X, this._points[i].Y, this._points[i+1].X,this._points[i+1].Y);
+            
         }
         
-        context.lineTo(point.X, point.Y);
-        
-        context.lineCap = 'round';
-        context.lineJoin = 'round';
-        
         context.stroke();
-        
-        this._lastPt = point;
+        context.closePath();
     }
     
     onStopDrawing(paper:paper.Paper, point:point.Point) {
         super.onStopDrawing(paper, point);
         
-        paper.baseLayer.getContext().closePath();
+        this._layer.copyTo(paper.baseLayer);
+        paper.removeLayer(this._layer);
     }
 }
