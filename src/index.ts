df967880aeb348a9981bda3edef053f221457d9d
PaoloCifariello <paolocifa@gmail.com>
2014-04-10 22:21:54 +0200

    Rimosso codice inutile

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,7 @@ import glob = require('./classes/Global');
 import pt = require('./classes/Point');
 import tool = require('./classes/Tool');
 import extension = require('./classes/Extension');
-/*
-import toolPencil = require('./extensions/Pencil/Pencil');
-import toolPen = require('./extensions/Pen/Pen');
-import toolEraser = require('./extensions/Eraser/Eraser');
-import toolBrush = require('./extensions/Brush/Brush');
-import toolShapeDrawer = require('./extensions/ShapeDrawer/ShapeDrawer');
 
-import toolColorPicker = require('./extensions/ColorPicker/ColorPicker');
-
-import extColorChooser = require('./extensions/ColorChooser/ColorChooser');
-import extSizeChooser = require('./extensions/SizeChooser/SizeChooser');
-import extZoom = require('./extensions/Zoom/Zoom');
-import extImageSaver = require('./extensions/ImageSaver/ImageSaver');
-import extPaperSize = require('./extensions/PaperSize/PaperSize');
-*/
 // node-webkit requires
 var gui = require('nw.gui');
 var paint:glob.Paint;
@@ -48,8 +34,6 @@ $(document).ready(function() {
     
     // Load Extensions and Tools
     paint.extensionManager.addExtensions();
-    //loadExtensions();
-        
 });
 
 /**
@@ -137,54 +121,4 @@ function createMenu()
     menu.append(mnuFile);
 
     gui.Window.get().menu = menu;
-}
-
-function loadExtensions() {
-    /*
-    var extensions : extension.Extension[] = [];
-    var defaultTool : tool.Tool;
-    
-    // Registra il tool Pencil
-    extensions.push(new toolPencil.Pencil(paint));
-    
-    // Registra il tool Pen
-    defaultTool = new toolPen.Pen(paint);
-    extensions.push(defaultTool);
-    
-    // Registra il tool Eraser
-    extensions.push(new toolEraser.Eraser(paint));
-    
-    // Registra il tool Brush
-    extensions.push(new toolBrush.Brush(paint));
-    
-    // Registra il tool Line
-    extensions.push(new toolShapeDrawer.ShapeDrawer(paint));
-    
-    // Registra il tool ColorPicker
-    extensions.push(new toolColorPicker.ColorPicker(paint));
-    
-    // Registra l'estensione ColorChooser per la scelta dei colori
-    extensions.push(new extColorChooser.ColorChooser(paint));
-    
-    // Registra l'estensione SizeChooser per la scelta della dimensione
-    extensions.push(new extSizeChooser.SizeChooser(paint));
-    
-    // Registra l'estensione PaperSize
-    extensions.push(new extPaperSize.PaperSize(paint));
-    
-    // Registra l'estensione Zoom
-    extensions.push(new extZoom.Zoom(paint));
-    
-    // Registra l'estensione ImageSaver
-    extensions.push(new extImageSaver.ImageSaver(paint));
-    
-    
-    extensions.forEach(function(extension){
-        extension.init();
-    });
-    
-    // Setta il tool corrente
-    
-    paint.setCurrentTool(defaultTool, null);
-    */
 }
