c1d3b84d3016c84b0d46426ec4f33392caf33afa
Daniele Di Sarli <danieleds0@gmail.com>
2014-04-14 20:20:58 +0200

    Workaround bug menu su Windows (fixes #9), aggiungendo funzione Paint.refreshMenu(). Comunque, da indagare.

--- src/classes/Global.ts
+++ src/classes/Global.ts
@@ -10,61 +10,61 @@ import extensionManager = require('./ExtensionManager');
  * objects within the application.
  */
 export class Paint {
-    
-    private _$ : JQueryStatic;
-    
-    public document : Document;
-    
+
+    private _$: JQueryStatic;
+
+    public document: Document;
+
     /** Insieme di tutte le estensioni caricate */
-    private extensions : { [index: string]: any; } = {};
+    private extensions: { [index: string]: any; } = {};
     /** Sottoinsieme di 'extensions' che contiene solo le estensioni di tipo Tool */
-    private tools : { [index: string]: any; } = {};
-    
+    private tools: { [index: string]: any; } = {};
+
     /** Current Paper object (the one at the base of the level hierarchy) */
-    public currentPaper : paper.Paper;
-    
+    public currentPaper: paper.Paper;
+
     /** Bar Manager */
-    public barManager : barManager.BarManager;
-   
+    public barManager: barManager.BarManager;
+
     /** Extension Manager */
-    public extensionManager : extensionManager.ExtensionManager;
-    
+    public extensionManager: extensionManager.ExtensionManager;
+
     /** Current colors */
-    public _primaryColor : color.Color;
-    public _secondaryColor : color.Color;
-    public _toolSize : number;
+    public _primaryColor: color.Color;
+    public _secondaryColor: color.Color;
+    public _toolSize: number;
     public File;
     public FileList;
     public menu = { File: null, Help: null };
-        
-    private _currentTool : any = null;
-    
-    constructor($ : JQueryStatic, document : Document) {
+
+    private _currentTool: any = null;
+
+    constructor($: JQueryStatic, document: Document) {
         this._$ = $;
         this.document = document;
-        
+
         this.currentPaper = new paper.Paper(this, $('#paper')[0]);
-        
+
         this.barManager = new barManager.BarManager(this);
         this.extensionManager = new extensionManager.ExtensionManager(this);
-        
+
         this._primaryColor = color.Color.Black;
         this._secondaryColor = color.Color.White;
     }
-    
-    get $():JQueryStatic {
+
+    get $(): JQueryStatic {
         return this._$;
     }
-    
-    registerExtension(instance : any) {
+
+    registerExtension(instance: any) {
         this.extensions[instance.EXTENSION_NAME] = instance;
     }
-    
-    registerTool(instance : any) {
+
+    registerTool(instance: any) {
         this.tools[instance.EXTENSION_NAME] = instance;
     }
-    
-    set primaryColor(value : color.Color) {
+
+    set primaryColor(value: color.Color) {
         this._primaryColor = value;
 
         this.forEachExtension(function (ext) {
@@ -73,12 +73,12 @@ export class Paint {
             }
         });
     }
-    
+
     get primaryColor() {
         return this._primaryColor;
     }
-    
-    set secondaryColor(value : color.Color) {
+
+    set secondaryColor(value: color.Color) {
         this._secondaryColor = value;
 
         this.forEachExtension(function (ext) {
@@ -91,8 +91,8 @@ export class Paint {
     get secondaryColor() {
         return this._secondaryColor;
     }
-    
-    set toolSize(value:number) {
+
+    set toolSize(value: number) {
         this._toolSize = value;
         this.forEachExtension(function (ext) {
             if (ext.onToolSizeChanged) {
@@ -100,38 +100,46 @@ export class Paint {
             }
         });
     }
-    
+
     get toolSize() {
         return this._toolSize;
     }
-    
+
     /**
      * Change active Tool
      * \param tool the new tool to be activated
      * \param idElement the element that caused tool activation
      */
-    setCurrentTool(tool : any, idElement : string) {
+    setCurrentTool(tool: any, idElement: string) {
         /* Set new tool and call Activated() and Deactivated() if tool listen these events */
         if (this._currentTool !== null && this._currentTool.deactivated) {
             this._currentTool.deactivated();
         }
-        
+
         this._currentTool = tool;
 
         if (this._currentTool.activated) {
             this._currentTool.activated(idElement);
         }
     }
-    
-    get currentTool() : any {
+
+    get currentTool(): any {
         return this._currentTool;
     }
 
-    public forEachExtension(callback: (ext:any) => void) {
+    public forEachExtension(callback: (ext: any) => void) {
         for (var ext in this.extensions) {
             if (this.extensions.hasOwnProperty(ext)) {
                 callback(this.extensions[ext]);
             }
         }
+    }
+
+    public refreshMenu(): void {
+        var gui = global.window.nwDispatcher.requireNwGui();
+        var oldmenu = gui.Window.get().menu;
+        oldmenu.append(this.menu.Help);
+        oldmenu.removeAt(oldmenu.items.length - 1);
+        gui.Window.get().menu = oldmenu;
     }
 }
--- src/extensions/ImageSaver/ImageSaver.ts
+++ src/extensions/ImageSaver/ImageSaver.ts
@@ -5,69 +5,69 @@ var fs = require('fs');
 
 var gui = global.window.nwDispatcher.requireNwGui();
 
-class ImageSaver
-{
-    public EXTENSION_NAME : string = "com.paintjs.ImageSaver";
-    paint : glob.Paint;
-    
-    private saveFileDialog : HTMLInputElement;
-    
-    public constructor(paint:glob.Paint) {
+class ImageSaver {
+    public EXTENSION_NAME: string = "com.paintjs.ImageSaver";
+    paint: glob.Paint;
+
+    private saveFileDialog: HTMLInputElement;
+
+    public constructor(paint: glob.Paint) {
         this.paint = paint;
     }
-    
+
     init() {
         var paint = this.paint,
             $ = this.paint.$,
             _this = this;
-        
+
         // Creates input file dialog
-        this.saveFileDialog = <HTMLInputElement> $('<input id="saveAs" type="file" nwsaveas accept=".png" />')[0]; 
-        
+        this.saveFileDialog = <HTMLInputElement> $('<input id="saveAs" type="file" nwsaveas accept=".png" />')[0];
+
         // Create save submenu
-        var mnuSave = new gui.MenuItem({ label: 'Save' })
-        
+        var mnuSave = new gui.MenuItem({ label: 'Save' });
+
         // add handler for click
-        mnuSave.click = function() {
-            _this.chooseFile(_this.saveFileDialog, function(filename) {
-                if(filename != "") {
+        mnuSave.click = function () {
+            _this.chooseFile(_this.saveFileDialog, function (filename) {
+                if (filename !== "" && filename !== null) {
                     var image = paint.currentPaper.getCanvas().toDataURL();
-                    image = image.replace(/^data:image\/png;base64,/,"");
-                    
-                    fs.writeFile(filename, image, "base64" , function(err) {
-                        if(err) {
+                    image = image.replace(/^data:image\/png;base64,/, "");
+
+                    fs.writeFile(filename, image, "base64", function (err) {
+                        if (err) {
                             console.log(err);
                         }
-                    });   
+                    });
                 }
             });
-        }
+        };
 
         this.paint.menu.File.submenu.append(mnuSave);
-    }    
-    
-    private chooseFile(dialog:HTMLInputElement, callback) {
+        this.paint.refreshMenu();
+    }
+
+    private chooseFile(dialog: HTMLInputElement, callback) {
         var paint = this.paint,
             $ = this.paint.$;
-        
+
         // Reset the FileDialog so that we can receive *change* events
         // even when the file stays the same.
         var fl = new paint.FileList();
         fl.append(new paint.File('', ''));
         fl.append(new paint.File('', 'no_file_selected_...\|/*?'));
         dialog.files = fl;
-        
-        
+
+
         var chooser = $(dialog);
-        
-        var change = function(evt) {
+
+        var change = function (evt) {
             $(this).off("change", change);
             callback($(this).val());
-        }
-        
+        };
+
         chooser.on("change", change);
         chooser.trigger('click');
-    }    
+    }
 }
 
 exports.Extensions = new Array();
